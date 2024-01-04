fe1134151fd6866f81a5feed3af43f5dc3d1ce03
Jhony Arana <jharanza1986@gmail.com>
2024-01-04 01:53:59 -0500

    feat: Create a database to test and its user

---
+++ setup_mysql_test.sql
@@ -0,0 +1,12 @@
+-- Create the database to do the tests
+-- Create the user with privileges
+
+CREATE DATABASE IF NOT exists hbnb_test_db;
+
+USE hbnb_test_db;
+
+CREATE USER hbnb_test@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
