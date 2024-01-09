32342e4b2f9d0aaee519b3206361f36e4af8d348
Jhony Arana <jharanza1986@gmail.com>
2024-01-09 14:35:19 -0500

    feat: Add documentation and the pycodestyle

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,16 @@
 #! /usr/bin/python3
+''' module that create a basic server '''
 from flask import Flask
+
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def start():
+    ''' Method that show a message'''
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port="5000")
