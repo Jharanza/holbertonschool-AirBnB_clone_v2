c02dcffd698240b1dbbb0c4e310dd720b0511b8a
jhony arana <jharanza1986@gmail.com>
2024-02-02 18:53:04 -0500

    fix: delete trailing spaces

--- tests/test_console.py
+++ tests/test_console.py
@@ -27,35 +27,35 @@ class TestConsoleDocs(unittest.TestCase):
 
     def test_pep8_conformance_test_console(self):
         """Test that tests/test_console.py conforms to PEP8."""
-        
+
         # create an instance of PEP8
         pep8s = pep8.StyleGuide(quiet=True)
-        
+
         # Get information from the file test_console
         result = pep8s.check_files(['tests/test_console.py'])
-        
+
         # Check if numbers of errors is 0 or print message
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
     def test_console_module_docstring(self):
         """Test for the console.py module docstring"""
-        
+
         # Check if doc is different of None or print message
         self.assertIsNot(console.__doc__, None,
                          "console.py needs a docstring")
-        
+
         # Check if length of doc is mayor or equal to 1 or print message
         self.assertTrue(len(console.__doc__) >= 1,
                         "console.py needs a docstring")
 
     def test_HBNBCommand_class_docstring(self):
         """Test for the HBNBCommand class docstring"""
-        
+
         # Check if exists doc or print message
         self.assertIsNot(HBNBCommand.__doc__, None,
                          "HBNBCommand class needs a docstring")
-        
+
         # Check if length of doc is major or equal to 1 or print message
         self.assertTrue(len(HBNBCommand.__doc__) >= 1,
                         "HBNBCommand class needs a docstring")
