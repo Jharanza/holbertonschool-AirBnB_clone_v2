fa7428792a01b5e24a7f56633c5ae128c6859b74
jhony arana <jharanza1986@gmail.com>
2024-02-08 16:13:28 -0500

    fix change position of teardown route

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
-''' Module that fetch data from storage engine '''
+"""
+starts a Flask web application
+"""
+
 from flask import Flask, render_template
+from models import *
 from models import storage
-from models.state import State
-
-
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown_session(exc):
-    ''' Method that close the session '''
-    storage.close()
-
-
 @app.route('/states_list', strict_slashes=False)
-def display_states():
-    ''' Method that list all states sorted bt name '''
-    states = storage.all(State).values()
-    states = sorted(states,  key=lambda state: state.name)
+def states_list():
+    """display a HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
 
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
